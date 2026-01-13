80614ab9d29ac9b245dd75383f9becaba25a419a
MaxMartin-git <martin_huth@outlook.de>
2026-01-14 00:44:22 +0100

    alte Ordner gelöscht

--- Ablage Code/preprocessInput_alt.h
+++
@@ -1,9 +0,0 @@
-#ifndef PREPROCESSINPUT_H
-#define PREPROCESSINPUT_H
-
-#include <Arduino.h> // für String
-
-// Funktionsdeklaration
-bool preprocessInput(const String &req);//RequestResult preprocessInput(const String &req);
-
-#endif
